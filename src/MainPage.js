fcc8044d601ceac43faf52509203ce893e87a39d
j.rizk <25933983+Jay991@users.noreply.github.com>
2023-01-02 23:02:14 +0200

    Update MainPage.js

--- src/MainPage.js
+++ src/MainPage.js
@@ -1,50 +1,55 @@
 import React, { useState } from 'react';
+import { convert } from './convert';
+
+import './MainPage.css';
 
 function MainPage() {
   const [file, setFile] = useState(null);
   const [convertedFile, setConvertedFile] = useState(null);
   const [loading, setLoading] = useState(false);
 
+  function handleFileChange(event) {
+    setFile(event.target.files[0]);
+  }
+
   function convertFile() {
     // Show loading spinner
     setLoading(true);
 
-    // Read file and convert it to desired format using Perl script
+    // Read file and convert it to desired format using convert function
     const reader = new FileReader();
     reader.onload = () => {
       const input = reader.result;
-      const output = convertUsingPerlScript(input);
+      const output = convert(input);
       setConvertedFile(output);
       setLoading(false);
     };
     reader.readAsText(file);
   }
 
-  function convertUsingPerlScript(input) {
-    // TODO: Use Perl script to convert input to desired format
-  }
-
-  function downloadFile() {
-    // Create download link for converted file
-    const link = document.createElement('a');
-    link.href = URL.createObjectURL(new Blob([convertedFile]));
-    link.download = 'output.csv';
-    document.body.appendChild(link);
-    link.click();
-    document.body.removeChild(link);
-  }
-
   return (
-    <div>
-      <input type="file" onChange={event => setFile(event.target.files[0])} />
-      <button onClick={convertFile}>
-        {loading ? 'Converting...' : 'Convert'}
+    <div className="main-page">
+      <h1>Convert CSV File</h1>
+      <div className="file-input">
+        <label htmlFor="file">Upload file:</label>
+        <input type="file" id="file" onChange={handleFileChange} />
+      </div>
+      <button onClick={convertFile} disabled={!file}>
+        Convert
       </button>
-      <button style={{ display: convertedFile ? 'block' : 'none' }} onClick={downloadFile}>
-        File ready for download
-      </button>
+      {loading && <div className="loading-spinner" />}
+      {convertedFile && (
+        <a
+          href={URL.createObjectURL(new Blob([convertedFile]))}
+          download="converted.csv"
+          className="download-button"
+        >
+          Download
+        </a>
+      )}
     </div>
   );
 }
 
 export default MainPage;
+
